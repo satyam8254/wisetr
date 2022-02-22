55881998142c2c26716b3b5595bb4b38260ee851
satyam8254 <satyamraj8254@gmail.com>
2022-02-23 01:00:49 +0530

    first commit

---
+++ client/src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Body from "./homebody";
+function Home(){
+    return(
+        <div>
+            <Body/>
+        </div>
+    );
+}
+
+export default Home;
---
+++ client/src/components/homebody.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Homebody(){
+    return(
+        <div>
+            <h1>hello from main body</h1>
+        </div>
+    );
+}
+export default Homebody;
