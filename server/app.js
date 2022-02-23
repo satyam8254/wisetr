5b9d7a2fdb25468218cf53f7b9c15c207248dadc
satyam8254 <satyamraj8254@gmail.com>
2022-02-23 09:34:30 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ const env = require("dotenv");
 const userRoutes = require("./route/user");
 const userData = require("./userdata/userinfo");
 const User = require("./model/userdetail");
+const cors = require("cors");
+
 env.config();
 mongoose.connect(process.env.DATABASE);
 const PORT = process.env.PORT
 app = express();
-
+app.use(cors())
 app.get("/push",async(req,res)=>{
     const data = await User.insertMany(userData);
     res.json(data);
